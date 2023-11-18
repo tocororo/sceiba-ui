2b07da37e2b59b78f3641c5885f58877241c71d3
Rafael Martinez Estevez <malayibiri@gmail.com>
2023-11-18 04:46:07 -0500

    merge help

--- src/app/modules/apps/help/help.module.ts
+++ src/app/modules/apps/help/help.module.ts
@@ -5,11 +5,10 @@ import { HttpClient } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { MarkdownModule } from 'ngx-markdown';
+import { SceibaUiSharedModule } from 'src/app/shared/shared.module';
 import { HelpComponent } from '../../common/help/help/help.component';
 import { StaticPagesComponent } from '../../common/help/static-pages/static-pages.component';
 import { HelpRoutingModule } from './help-routing.module';
-import { SceibaUiCoreModule } from 'src/app/core/core.module';
-import { SceibaUiSharedModule } from 'src/app/shared/shared.module';
 
 export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
