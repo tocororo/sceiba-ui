5590bd3e6db1a57005c7127e062715ca9a65101d
Rafael Martinez Estevez <malayibiri@gmail.com>
2023-11-19 23:28:20 -0500

    build v0.0.1

---
+++ src/environments/environment.prod.sceiba.ts
@@ -0,0 +1,52 @@
+import { Environment } from 'toco-lib';
+
+class EnvironmentImpl implements Environment {
+  production = false;
+  sceibaHost = 'https://sceiba.reduniv.edu.cu/';
+  cuorHost = 'https://sceiba.reduniv.edu.cu/';
+  sceibaApi = 'https://sceiba.reduniv.edu.cu/api/';
+  cuorApi = 'https://sceiba.reduniv.edu.cu/api/';
+
+  appHost = 'https://sceiba.cu';
+  appName = 'Sceiba';
+
+  websiteUsername_Twitter = '@SceibaCuba';
+  websiteUsername_Facebook = '@sceiba';
+
+  oauthRedirectUri = 'https://sceiba.cu/';
+  oauthClientId = 'TNANDqWA5WP6EZ4iWFnMfQUk2T7A05r5ucfNnanx';
+  oauthScope = 'user:email';
+  topOrganizationPID = 'orgaid.223';
+  cachableUrls = [];
+
+  matomoUrl = 'https://crai-stats.upr.edu.cu/';
+  matomoSiteId = 7;
+
+  sceiba = '/';
+  discover = '/records';
+  catalog = '/catalog';
+  revistasmes = '/revistasmes';
+  organizations = '/organizations';
+  persons = '/persons';
+  vocabularies = 'https://vocabularios.sceiba.cu/';
+  moodle = 'https://courses.sceiba.org/';
+  evaluations = '/evaluations';
+
+
+  oauthInfo = {
+    serverHost: this.sceibaHost,
+    loginUrl: this.sceibaHost + 'oauth/internal/authorize',
+    tokenEndpoint: this.sceibaHost + 'oauth/token',
+    userInfoEndpoint: this.sceibaApi + 'profile/me',
+    appHost: this.appHost,
+    appName: this.appName,
+    oauthRedirectUri: this.oauthRedirectUri,
+    oauthClientId: this.oauthClientId,
+    oauthScope: this.oauthScope,
+  }
+
+}
+
+export const environment = new EnvironmentImpl();
+
+export const allowedURLS = ['https://sceiba.reduniv.edu.cu/api/'];
