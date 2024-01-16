6613b445c7a3fd90153aed44d3cfaf6d0aaa1d18
carlomonterrey <carlosmonterrey17@gmail.com>
2024-01-16 10:56:52 -0500

    init commit

---
+++ src/app/modules/apps/graph/graph.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'seiba-ui-graph',
+  templateUrl: './graph.component.html',
+  styleUrls: ['./graph.component.scss']
+})
+export class GraphComponent {
+
+}
---
+++ src/app/modules/apps/graph/graph.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GraphRoutingModule } from './graph-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GraphRoutingModule
+  ]
+})
+export class GraphModule { }
