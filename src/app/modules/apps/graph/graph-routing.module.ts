ad0de39e54c9a40856b92b4c845722b88c3aa7b7
carlomonterrey <carlosmonterrey17@gmail.com>
2024-01-16 11:22:45 -0500

    Create the main routing

--- src/app/modules/apps/graph/graph-routing.module.ts
+++ src/app/modules/apps/graph/graph-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GraphComponent } from './graph.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",
+  component:GraphComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class GraphRoutingModule { }
+
+
+export class GraphRoutingModule {
+ 
+   
+
+ }
--- src/app/modules/apps/graph/graph.module.ts
+++ src/app/modules/apps/graph/graph.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GraphRoutingModule } from './graph-routing.module';
+import { RouterModule } from '@angular/router';
+import { GraphComponent } from './graph.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [GraphComponent],
   imports: [
     CommonModule,
-    GraphRoutingModule
+    GraphRoutingModule,
+    RouterModule,
+    FlexLayoutModule
   ]
 })
 export class GraphModule { }
